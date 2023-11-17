a2a9a9f3794a609de67c327810fed49ae11d26ae
Michele Miotti <michele.miotti@mail.polimi.it>
2023-11-17 13:11:55 +0100

    Ser default directive for "real" type alias

--- src/FourierTransform.hpp
+++ src/FourierTransform.hpp
@@ -4,12 +4,16 @@
 
 #ifdef FLOAT
 using real = float;
-#endif
+#else
 #ifdef DOUBLE
 using real = double;
-#endif
+#else
 #ifdef LONG_DOUBLE
 using real = long double;
+#else
+using real = double;
+#endif
+#endif
 #endif
 
 //Perform the Fourier Transform of a sequence, using the O(n^2) algorithm
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,33 @@
 #include "FourierTransform.hpp"
 
 int main(int argc, char* argv[]) {
-	if(argc == 1) {
-		std::cout << "Hello world!" << std::endl;
-	} else if(argc == 2) {
-		std::cout << "Hello " << argv[1] << "!" << std::endl;
-	} else {
-		std::cerr << "Too many arguments!" << std::endl;
-	}
+	// Testing the argument parsing
+	if (argc == 1) { std::cout << "No arguments detected." << std::endl; } 
+	else if (argc == 2) { std::cout << "Argument detected: " << argv[1] << std::endl; }
+	else { std::cerr << "Too many arguments!" << std::endl; }
 	
-	/*
-	//A simple test on computing the Fourier Transform with the naive method
-	//Results correspond to those obtained running numpy.fft.fft([1,2,3]) in python
-	std::vector<std::complex<real>> example_sequence = {std::complex<real>{1.0,0.0}, std::complex<real>{2.0,0.0}, std::complex<real>{3.0,0.0}};
-	std::vector<std::complex<real>> output_sequence = FourierTransform(example_sequence);
-	for(size_t i=0; i<output_sequence.size(); i++) {
-		std::cout << output_sequence[i] << " ";
+	// Generating a sequence of complex numbers
+	std::vector<std::complex<real>> sequence;
+	sequence.reserve(16);
+
+	for (size_t i = 0; i < 16; i++)
+	{
+		// Add a random complex number to the sequence
+		sequence.emplace_back(rand() % 100, rand() % 100);
+
+		// Print the sequence
+		std::cout << sequence[i] << std::endl;
 	}
-	std::cout << std::endl;
-	*/
+
+	// Compute the O(n^2) Fourier Transform of the sequence
+	std::vector<std::complex<real>> dft_result = FourierTransform(sequence);
+
+	// Print the results
+	std::cout << "DFT result:" << std::endl;
+	for (size_t i = 0; i < dft_result.size(); i++)
+	{
+		std::cout << dft_result[i] << std::endl;
+	}
 	
 	return 0;
 }
