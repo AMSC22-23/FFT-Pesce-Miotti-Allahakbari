739d151deb708c2f4afb1c7183707b0713c8cab5
FrancescoPesce <francesco1.pesce@mail.polimi.it>
2023-11-24 09:54:08 +0100

    Remove old Makefile Change some comments and variable names in BitReversePerumation

--- src/BitReversePermutation.cpp
+++ src/BitReversePermutation.cpp
@@ -20,8 +20,7 @@ size_t BitReversePermutation(const size_t index, const size_t bitsize) {
 
 // Calculate the time needed to reverse the bits of the indexes of a sequence
 // of size "number_elements", expressed in microseconds.
-// Note that the compiler might remove the for loop if 03 is used.
-unsigned long CalculateTimeBitReversePermutation(size_t num_elements, unsigned int num_threads, size_t &result) {
+unsigned long CalculateTimeBitReversePermutation(size_t num_elements, unsigned int num_threads, size_t &O3_workaround) {
 	// Calculate the necessary number of bits to store the sequence.
 	size_t num_bits = static_cast<size_t>(log2(num_elements));
 	if (1 << num_bits != num_elements) num_bits++;
@@ -36,7 +35,7 @@ unsigned long CalculateTimeBitReversePermutation(size_t num_elements, unsigned i
 	#pragma omp parallel for
 	for (size_t i=0; i<num_elements; i++)
 	{
-		result = BitReversePermutation(i, num_bits);
+		O3_workaround = BitReversePermutation(i, num_bits);
 	}
 
 	// Return the time needed to reverse the bits of the indexes.
@@ -46,7 +45,7 @@ unsigned long CalculateTimeBitReversePermutation(size_t num_elements, unsigned i
 
 // A wrapper around the previous function to avoid O3 optimazation removing the for loop
 unsigned long CalculateTimeBitReversePermutation(size_t num_elements, unsigned int num_threads) {
-	size_t result;
+	size_t O3_workaround;
 
-	return CalculateTimeBitReversePermutation(num_elements, num_threads, result);
+	return CalculateTimeBitReversePermutation(num_elements, num_threads, O3_workaround);
 }
