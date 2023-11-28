f0b5df67d78a7ccd6bc8352ebe99601eb3280b69
Michele Miotti <michele.miotti@mail.polimi.it>
2023-11-28 08:45:41 +0100

    Maximize significant digit amount in .csv files

--- src/VectorExporter.cpp
+++ src/VectorExporter.cpp
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 // Utility function to write a sequence of complex numbers to a file.
 void WriteToFile(const std::vector<std::complex<real>> &sequence, const std::string &filename)
@@ -9,9 +10,10 @@ void WriteToFile(const std::vector<std::complex<real>> &sequence, const std::str
     // Open the file.
     std::ofstream file(filename);
 
-    // Write the sequence to the file in .csv format.
+    // Write the sequence to the file in .csv format, with full precision.
     for (size_t i = 0; i < sequence.size(); i++)
     {
+        file << std::setprecision(std::numeric_limits<real>::max_digits10);
         file << sequence[i].real() << "," << sequence[i].imag() << std::endl;
     }
 
