26bac13cff5c6fd86495af0739e02d86cec06226
mh2033 <aakbarimh@gmail.com>
2024-01-01 19:38:49 +0100

    Fix boundary checking problems

--- src/FFTGPU.cu
+++ src/FFTGPU.cu
@@ -2,31 +2,23 @@
 
 #include "FFTGPU.hpp"
 
-#define TILE_SIZE 32
+#define TILE_SIZE 256
 
-#define TILE_DIM 32
-#define BLOCK_ROWS 8
+#define BLOCK_SIZE 32
 
 using namespace FourierTransform;
 
 // CUDA kernel for transposing a 2D array efficiently using shared memory
-__global__ void transpose(const cuda::std::complex<real>* idata,
-                          cuda::std::complex<real>* odata) {
-  __shared__ cuda::std::complex<real> tile[TILE_DIM * (TILE_DIM + 1)];
+__global__ void transpose(cuda::std::complex<real>* input,
+                          cuda::std::complex<real>* output, int n) {
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  int x = blockIdx.x * TILE_DIM + threadIdx.x;
-  int y = blockIdx.y * TILE_DIM + threadIdx.y;
-  int width = gridDim.x * TILE_DIM;
-
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-    tile[(threadIdx.y + j) * TILE_DIM + threadIdx.x] =
-        idata[(y + j) * width + x];
-
-  __syncthreads();
-
-  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
-    odata[(y + j) * width + x] =
-        tile[(threadIdx.y + j) * TILE_DIM + threadIdx.x];
+  if (x < n && y < n) {
+    int in_index = y * n + x;
+    int out_index = x * n + y;
+    output[out_index] = input[in_index];
+  }
 }
 
 /**
@@ -50,21 +42,23 @@ __global__ void fft1d(cuda::std::complex<real>* data, int size, int m,
   size_t j = tid % half_m;
   const size_t k_plus_j = k + j;
 
-  const cuda::std::complex<real> omega =
-      cuda::std::exp(cuda::std::complex<real>{0, base / half_m * j});
-  const cuda::std::complex<real> t = omega * data[k_plus_j + half_m];
-  const cuda::std::complex<real> u = data[k_plus_j];
-  const cuda::std::complex<real> even = u + t;
-  const cuda::std::complex<real> odd = u - t;
-  data[k_plus_j] = even;
-  data[k_plus_j + half_m] = odd;
+  if ((k_plus_j + half_m) < size) {
+    const cuda::std::complex<real> omega =
+        cuda::std::exp(cuda::std::complex<real>{0, base / half_m * j});
+    const cuda::std::complex<real> t = omega * data[k_plus_j + half_m];
+    const cuda::std::complex<real> u = data[k_plus_j];
+    const cuda::std::complex<real> even = u + t;
+    const cuda::std::complex<real> odd = u - t;
+    data[k_plus_j] = even;
+    data[k_plus_j + half_m] = odd;
+  }
 }
 
 void FourierTransform::run_fft_gpu(
     cuda::std::complex<FourierTransform::real>* data, int n, int m,
     FourierTransform::real base, cudaStream_t stream_id) {
   int block_dim = TILE_SIZE;
-  int grid_dim = (n + TILE_SIZE) / (TILE_SIZE * 2);
+  int grid_dim = (n + TILE_SIZE - 1) / (TILE_SIZE);
   fft1d<<<grid_dim, block_dim, 0, stream_id>>>(data, n, m, base);
 }
 
@@ -72,7 +66,7 @@ void FourierTransform::bitreverse_gpu(cuda::std::complex<real>* in,
                                       cuda::std::complex<real>* out, int size,
                                       int s, cudaStream_t stream_id) {
   int block_dim = TILE_SIZE;
-  int grid_dim = size / TILE_SIZE;
+  int grid_dim = (size + TILE_SIZE - 1) / (TILE_SIZE);
 
   bitrev_reorder<<<grid_dim, block_dim, 0, stream_id>>>(in, out, s);
 }
@@ -80,8 +74,9 @@ void FourierTransform::bitreverse_gpu(cuda::std::complex<real>* in,
 void FourierTransform::transpose_gpu(cuda::std::complex<real>* in,
                                      cuda::std::complex<real>* out, int n,
                                      cudaStream_t stream_id) {
-  dim3 block_dim(TILE_DIM, BLOCK_ROWS);
-  dim3 grid_dim(n / TILE_DIM, n / BLOCK_ROWS);
+  dim3 block_dim(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 grid_dim((n + BLOCK_SIZE - 1) / BLOCK_SIZE,
+                (n + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
-  transpose<<<grid_dim, block_dim, 0, stream_id>>>(in, out);
+  transpose<<<grid_dim, block_dim, 0, stream_id>>>(in, out, n);
 }
