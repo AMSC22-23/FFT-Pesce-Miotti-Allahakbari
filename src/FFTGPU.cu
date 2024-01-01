5395a88b84d495b82fd36dfbc995e8c476497aad
mh2033 <aakbarimh@gmail.com>
2024-01-01 17:54:44 +0100

    Add GPU matrix transpose method

--- src/FFTGPU.cu
+++ src/FFTGPU.cu
@@ -2,9 +2,32 @@
 
 #include "FFTGPU.hpp"
 
-#define TILE_SIZE 1024
+#define TILE_SIZE 32
+
+#define TILE_DIM 32
+#define BLOCK_ROWS 8
 
 using namespace FourierTransform;
+
+// CUDA kernel for transposing a 2D array efficiently using shared memory
+__global__ void transpose(const cuda::std::complex<real>* idata,
+                          cuda::std::complex<real>* odata) {
+  __shared__ cuda::std::complex<real> tile[TILE_DIM * (TILE_DIM + 1)];
+
+  int x = blockIdx.x * TILE_DIM + threadIdx.x;
+  int y = blockIdx.y * TILE_DIM + threadIdx.y;
+  int width = gridDim.x * TILE_DIM;
+
+  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+    tile[(threadIdx.y + j) * TILE_DIM + threadIdx.x] =
+        idata[(y + j) * width + x];
+
+  __syncthreads();
+
+  for (int j = 0; j < TILE_DIM; j += BLOCK_ROWS)
+    odata[(y + j) * width + x] =
+        tile[(threadIdx.y + j) * TILE_DIM + threadIdx.x];
+}
 
 /**
  * Reorders array by bit-reversing the indexes.
@@ -18,8 +41,8 @@ __global__ void bitrev_reorder(cuda::std::complex<real>* in,
     out[__brevll(id) >> (64 - s)] = in[id];
 }
 
-__global__ void fft(cuda::std::complex<real>* data, int size, int m,
-                    real base) {
+__global__ void fft1d(cuda::std::complex<real>* data, int size, int m,
+                      real base) {
   int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
   int half_m = m >> 1;
@@ -38,18 +61,27 @@ __global__ void fft(cuda::std::complex<real>* data, int size, int m,
 }
 
 void FourierTransform::run_fft_gpu(
-    cuda::std::complex<FourierTransform::real>* data, int size, int m,
-    FourierTransform::real base) {
+    cuda::std::complex<FourierTransform::real>* data, int n, int m,
+    FourierTransform::real base, cudaStream_t stream_id) {
   int block_dim = TILE_SIZE;
-  int grid_dim = (size + TILE_SIZE) / (TILE_SIZE * 2);
-  fft<<<grid_dim, block_dim>>>(data, size, m, base);
+  int grid_dim = (n + TILE_SIZE) / (TILE_SIZE * 2);
+  fft1d<<<grid_dim, block_dim, 0, stream_id>>>(data, n, m, base);
 }
 
 void FourierTransform::bitreverse_gpu(cuda::std::complex<real>* in,
                                       cuda::std::complex<real>* out, int size,
-                                      int s) {
+                                      int s, cudaStream_t stream_id) {
   int block_dim = TILE_SIZE;
   int grid_dim = size / TILE_SIZE;
 
-  bitrev_reorder<<<grid_dim, block_dim>>>(in, out, s);
+  bitrev_reorder<<<grid_dim, block_dim, 0, stream_id>>>(in, out, s);
+}
+
+void FourierTransform::transpose_gpu(cuda::std::complex<real>* in,
+                                     cuda::std::complex<real>* out, int n,
+                                     cudaStream_t stream_id) {
+  dim3 block_dim(TILE_DIM, BLOCK_ROWS);
+  dim3 grid_dim(n / TILE_DIM, n / BLOCK_ROWS);
+
+  transpose<<<grid_dim, block_dim, 0, stream_id>>>(in, out);
 }
